5088fecc45f6d84687320fde8fbc536830fa761d
Jason <jasoneperreault@yahoo.com>
2013-08-21 12:13:14 -0400

    Pushing up extrusion of SVG shapes.

---
+++ svgToThreejs.js
@@ -0,0 +1,222 @@
+var currentPoint, numberLength;
+var descArray = [];
+
+function svgToThreeD(svgString)
+{
+	var toop = [];
+	var shapes = [];
+	var svgIndexer = svgString;
+	var shapeNumber = 0;
+	
+	while (svgIndexer.indexOf('d="m') !== -1)
+	{
+		toop[shapeNumber] = addToString (svgIndexer.indexOf('d="m') + 4, svgIndexer);
+		svgIndexer = svgIndexer.substr(svgIndexer.indexOf('d="m') + 4, svgIndexer.length);
+		shapeNumber++;
+	}
+		
+	for (var i = 0; i < toop.length; i++)
+	{
+		shapes[i] = svgDescToThreejs(toop[i]);
+	}
+	
+	return shapes;
+}
+
+function svgDescToThreejs(lines)
+{
+	var weLoveJason = new THREE.Shape();
+	weLoveJason.moveTo(lines[0].x, lines[0].y)
+	for (var i = 1; i < lines.length; i++)
+	{
+		if (lines[i].type === 'lineTo')
+			weLoveJason.lineTo(lines[i].x, lines[i].y);
+		else
+			weLoveJason.bezierCurveTo(lines[i].cp1x, lines[i].cp1y, lines[i].cp2x, lines[i].cp2y, lines[i].x, lines[i].y);
+	}
+	
+	return weLoveJason;
+}
+
+function addToString(startingIndex, svgString)
+{
+	currentPoint = startingIndex;
+	var endPoint, type, desc;
+	desc = new svgDesc();
+	descArray = [];
+	numberLength = 0;
+	
+	for (var i = currentPoint; i < svgString.length; i++)
+		if (svgString[i] == "z")
+		{
+			endPoint = i;
+			break;
+		}
+	
+	desc = parseMoveTo(desc, svgString);
+	descArray.push(desc);
+	
+	if (svgString[currentPoint-1] === 'l')
+		type = 'lineTo';
+	else if (svgString[currentPoint-1] === 'c')
+		type = 'bezierCurveTo';
+		
+	while (currentPoint < endPoint)
+	{	
+		desc = new svgDesc();
+		if (type === 'lineTo')
+		{
+			desc = parseLineTo(desc, svgString);
+			descArray.push(desc);
+		}
+		else if (type === 'bezierCurveTo')
+		{
+			desc = parseBezierCurveTo(desc, svgString);
+			descArray.push(desc);
+		}
+		
+		if (svgString[currentPoint-1] === 'l')
+			type = 'lineTo';
+		else if (svgString[currentPoint-1] === 'c')
+			type = 'bezierCurveTo';
+	}
+	
+	return descArray;
+}
+
+function parseMoveTo(desc, svgString)
+{
+	while (svgString[currentPoint + numberLength] !== ',')
+	{
+		numberLength++;
+	}
+		
+	desc.x = parseInt(svgString.substr(currentPoint, numberLength));
+	console.log('Desc X Before: ',desc.x);
+	console.log('Desc X After: ',desc.x);
+	currentPoint += numberLength + 1;
+	numberLength = 0;
+
+	while (svgString[currentPoint + numberLength] !== 'l' && svgString[currentPoint + numberLength] !== 'z' && svgString[currentPoint + numberLength] !== 'c')
+	{
+		numberLength++;
+	}
+	
+	desc.y = parseInt(svgString.substr(currentPoint, numberLength));
+	console.log('Desc Y Before: ',desc.y);
+	desc.y -= 140;
+	desc.y /= -1.75;
+	console.log('Desc Y After: ',desc.y);
+	desc.type = 'moveTo';
+	currentPoint += numberLength + 1;
+	numberLength = 0;
+	
+	return desc;
+}
+
+function parseLineTo(desc, svgString)
+{
+	while (svgString[currentPoint + numberLength] !== ',')
+	{
+		numberLength++;
+	}
+		
+	desc.x = parseInt(svgString.substr(currentPoint, numberLength));
+	currentPoint += numberLength + 1;
+	numberLength = 0;
+
+	while (svgString[currentPoint + numberLength] !== 'l' && svgString[currentPoint + numberLength] !== 'z' && svgString[currentPoint + numberLength] !== 'c')
+	{
+		numberLength++;
+	}
+	
+	desc.y = parseInt(svgString.substr(currentPoint, numberLength));
+	desc.type = 'lineTo';
+	currentPoint += numberLength + 1;
+	numberLength = 0;
+		 
+	desc.x = descArray[descArray.length-1].x + desc.x;
+	desc.y = descArray[descArray.length-1].y - desc.y;
+	
+	return desc;
+}
+
+function parseBezierCurveTo(desc, svgString)
+{
+	while (svgString[currentPoint + numberLength] !== ',')
+	{
+		numberLength++;	
+	}
+	desc.cp1x = parseInt(svgString.substr(currentPoint, numberLength));
+	currentPoint += numberLength + 1;
+	numberLength = 0;
+
+	while (svgString[currentPoint + numberLength] !== ' ')
+	{
+		numberLength++;
+	}
+
+	desc.cp1y = parseInt(svgString.substr(currentPoint, numberLength));
+	currentPoint += numberLength + 1;
+	numberLength = 0;
+	
+	while (svgString[currentPoint + numberLength] !== ',')
+	{
+		numberLength++;	
+	}
+		
+	desc.cp2x = parseInt(svgString.substr(currentPoint, numberLength));
+	currentPoint += numberLength + 1;
+	numberLength = 0;
+
+	while (svgString[currentPoint + numberLength] !== ' ')
+	{
+		numberLength++;
+	}
+	
+	desc.cp2y = parseInt(svgString.substr(currentPoint, numberLength));
+	currentPoint += numberLength + 1;
+	numberLength = 0;
+	
+	while (svgString[currentPoint + numberLength] !== ',')
+	{
+		numberLength++;	
+	}
+		
+	desc.x = parseInt(svgString.substr(currentPoint, numberLength));
+	currentPoint += numberLength + 1;
+	numberLength = 0;
+
+	while (svgString[currentPoint + numberLength] !== 'l' && svgString[currentPoint + numberLength] !== 'z' && svgString[currentPoint + numberLength] !== 'c')
+	{
+		numberLength++;
+	}
+	
+	desc.y = parseInt(svgString.substr(currentPoint, numberLength));
+	desc.type = 'bezierCurveTo';
+	currentPoint += numberLength + 1;
+	numberLength = 0;
+	
+	desc.cp1x = descArray[descArray.length-1].x + desc.cp1x;
+	desc.cp1y = descArray[descArray.length-1].y - desc.cp1y;
+	desc.cp2x = descArray[descArray.length-1].x + desc.cp2x;
+	desc.cp2y = descArray[descArray.length-1].y - desc.cp2y;
+	desc.x = descArray[descArray.length-1].x + desc.x;
+	desc.y = descArray[descArray.length-1].y - desc.y;
+	
+	return desc;
+}
+
+var svgDesc = function(x, y, type, controlPointOneX, controlPointOneY, controlPointTwoX, controlPointTwoY)
+{
+	this.x = x;
+	this.y = y;
+	this.type = type;
+	this.cp1x = controlPointOneX;
+	this.cp1y = controlPointOneY;
+	this.cp2x = controlPointTwoX;
+	this.cp2y = controlPointTwoY;
+}
+
+//svgCanvas.getSvgString()(function(data, error) {test = data})
+//svgToThreeD(test)
---
+++ threejsShape.js
@@ -0,0 +1,140 @@
+var container, stats;
+
+var camera, scene, renderer;
+
+var text, plane;
+
+var targetRotation = 0;
+var targetRotationOnMouseDown = 0;
+
+var mouseX = 0;
+var mouseXOnMouseDown = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+init();
+animate();
+
+function init() {
+
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+	container.style.zIndex = 100;
+
+	camera = new THREE.PerspectiveCamera( 10, window.innerWidth / window.innerHeight, 500, 10000 );
+	camera.position.z = 1000;
+	// camera = new THREE.PerspectiveCamera( 4, window.innerWidth / window.innerHeight, 500, 100000 );
+	// camera.position.z = 1000;
+
+	scene = new THREE.Scene();
+
+	var light = new THREE.DirectionalLight( 0xffffff );
+	light.position.set( 0, 0, 1 );
+	scene.add( light );
+
+	parent = new THREE.Object3D();
+	parent.position.y = 50;
+	scene.add( parent );
+	
+	var triangleShape = new THREE.Shape();
+	triangleShape.moveTo(  80, 20 );
+	triangleShape.lineTo(  40, 80 );
+	triangleShape.lineTo( 120, 80 );
+	triangleShape.lineTo(  80, 20 ); 
+
+	var extrudeSettings = { amount: 50 }; // bevelSegments: 2, steps: 2 , bevelSegments: 5, bevelSize: 8, bevelThickness:5
+	extrudeSettings.bevelEnabled = true;
+	extrudeSettings.bevelSegments = 2;
+	extrudeSettings.steps = 2;
+	addShape( triangleShape, extrudeSettings, 0xffee00, -180, 0, 0, 0, 0, 0, 1 );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	container.appendChild( renderer.domElement );
+
+	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+	document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+	document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseDown( event ) {
+	event.preventDefault();
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+	mouseXOnMouseDown = event.clientX - windowHalfX;
+	targetRotationOnMouseDown = targetRotation;
+
+}
+
+function onDocumentMouseMove( event ) {
+	mouseX = event.clientX - windowHalfX;
+	targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
+}
+
+function onDocumentMouseUp( event ) {
+	document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+}
+
+function onDocumentMouseOut( event ) {
+	document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+}
+
+function onDocumentTouchStart( event ) {
+
+	if ( event.touches.length == 1 ) {
+		event.preventDefault();
+		mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
+		targetRotationOnMouseDown = targetRotation;
+	}
+}
+
+function onDocumentTouchMove( event ) {
+
+	if ( event.touches.length == 1 ) {
+		event.preventDefault();
+		mouseX = event.touches[ 0 ].pageX - windowHalfX;
+		targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
+	}
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+	render();
+}
+
+function render() {
+	parent.rotation.y += ( targetRotation - parent.rotation.y ) * 0.05;
+	renderer.render( scene, camera );
+}
+
+function addShape( shape, extrudeSettings, color, x, y, z, rx, ry, rz, s ) {
+		var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+		var mesh = THREE.SceneUtils.createMultiMaterialObject( geometry, [ new THREE.MeshLambertMaterial( { color: color } ), new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, transparent: true } ) ] );
+		mesh.position.set( x, y, z - 75 );
+		mesh.rotation.set( rx, ry, rz );
+		mesh.scale.set( s, s, s );
+		parent.add( mesh );
+	}
